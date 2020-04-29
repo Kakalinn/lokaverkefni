d5bcce256cefffa8d84bd1b47c1d11abb09410de
Bergur Snorrason <bsnorrason@gmail.com>
2020-04-29 08:04:01 +0000

    fdsa

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pdflatex -output-directory output verkefni &> out
-cd output
-bibtex verkefni &> /dev/null
-cd ..
-pdflatex -output-directory output verkefni &> out
-pdflatex -output-directory output verkefni &> out
+#cd output
+#bibtex verkefni &> /dev/null
+#cd ..
+#pdflatex -output-directory output verkefni &> out
+#pdflatex -output-directory output verkefni &> out
